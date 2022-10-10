cba4bb71e5c831510b7a5c0f433bfd98bd2ade0e
curse <58047023+cursecodes@users.noreply.github.com>
2022-10-10 17:17:19 -0600

    Fix spelling mistake

--- pages/index.js
+++ pages/index.js
@@ -16,7 +16,7 @@ export default function Home() {
         <div>
           <Checkbox color="pink" pt={30} label="Fuel airplane" />
           <Checkbox color="pink" pt={8} label="Do walkaround" />
-          <Checkbox color="pink" pt={8} label="Pull flihgt tags" />
+          <Checkbox color="pink" pt={8} label="Pull flight tags" />
           <Checkbox color="pink" pt={8} label="Power on GPU" />
           <Checkbox color="pink" pt={8} label="Power on APU" />
           <Checkbox color="pink" pt={8} label="Power on plane" />
